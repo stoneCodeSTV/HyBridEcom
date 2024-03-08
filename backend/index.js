95e733220c0d7bcca73cb602dfc0aee025dc623e
Steeve <steeve.tassy@gmail.com>
2024-03-08 16:33:41 -0500

    added admin folder

--- backend/index.js
+++ backend/index.js
@@ -72,9 +72,23 @@ const Product = mongoose.model('Product', {
     }
 })
 
+//API to ADD Products
 app.post('/addproduct', async(req,res) =>{
+        let products = await Product.find({});
+        let id;
+        if(products.length>0)
+        {
+            let last_product_array= products.slice(-1);
+            let last_product = last_product_array[0];
+            id = last_product.id+1;
+        } 
+        else
+        {
+            id=1;
+        }
+
         const product = new Product({
-            id:req.body.id,
+            id:id,
             name:req.body.name,
             image:req.body.image, 
             category:req.body.category,
@@ -87,6 +101,23 @@ app.post('/addproduct', async(req,res) =>{
             success:true,
             name:req.body.name
         })
+})
+
+//API to DELETE Products
+app.post('/removeproduct',async(req,res) => {
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log('Removed');
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+//Creating API to get ALLPRODUCTS
+app.get('/allproducts', async(req,res) => {
+    let products = await Product.find({});
+    console.log('All Products Fetched');
+    res.send(products);
 })
 
 
