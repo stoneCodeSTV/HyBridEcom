cfd4429ca14afe7503b648212ce2d431a5eb6502
Steeve <steeve.tassy@gmail.com>
2024-03-08 15:41:35 -0500

    added product schema and created endpoints

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,84 @@ app.use(express.json());
 app.use(cors());
 
 //DB Connection with MongoDB 
-mongoose.connect("mongodb+srv://omerta1:create.envfile@cluster0.7eduzss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://omerta1:4MomYS9KXDCsMThe@cluster0.7eduzss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 
-//API 
+//API Creation 
 
 app.get( '/', (req,res) => {
     res.send('App is Running')
 })
+
+//Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+//Creating Endpoint for uploaded images
+app.use('/images', express.static('upload/images'))
+
+app.post('/upload', upload.single('product'), (req,res) => {
+    res.json({
+        success:1, 
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Schema for Products 
+const Product = mongoose.model('Product', {
+    id: {
+        type:Number, 
+        required:true
+    }, 
+    name: {
+        type:String, 
+        required:true
+    },
+    image: {
+        type:String,
+        required:true
+    },
+    category: {
+        type:String,
+        required:true
+    }, 
+    price: {
+        type:Number,
+        required:true
+    },
+    date: {
+        type:Date,
+        default:Date.now
+    },
+    availabe: {
+        type: Boolean,
+        default: true
+    }
+})
+
+app.post('/addproduct', async(req,res) =>{
+        const product = new Product({
+            id:req.body.id,
+            name:req.body.name,
+            image:req.body.image, 
+            category:req.body.category,
+            price:req.body.price,
+        });
+        console.log(product);
+        await product.save();
+        console.log('Saved'); 
+        res.json({
+            success:true,
+            name:req.body.name
+        })
+})
+
 
 app.listen(port, (error) => {
     if (!error) {
